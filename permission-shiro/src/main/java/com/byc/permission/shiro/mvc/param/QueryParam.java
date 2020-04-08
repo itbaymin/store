f4addf32ecc9002ee65fb262e4d56684643298ab
baiyongcheng <18215637262@163.com>
2020-04-08 20:45:40 +0800

    修改

--- permission-shiro/src/main/java/com/byc/permission/shiro/mvc/param/QueryParam.java
+++ permission-shiro/src/main/java/com/byc/permission/shiro/mvc/param/QueryParam.java
@@ -2,6 +2,7 @@ package com.byc.permission.shiro.mvc.param;
 
 import com.byc.common.utils.TimeFormatter;
 import lombok.Data;
+import org.springframework.util.StringUtils;
 
 @Data
 /**公共查询参数*/
@@ -9,14 +10,24 @@ public class QueryParam {
     private String start;
     private String end;
     private String cond;
+    private String sort;    //排序方式
+    private String field;   //排序字段
     private int limit;
     private int page;
 
     public Long getStart() {
+        if(StringUtils.isEmpty(start))
+            return null;
         return TimeFormatter.formatStr(TimeFormatter.Format.SIMPLE,start);
     }
 
     public Long getEnd() {
+        if(StringUtils.isEmpty(end))
+            return null;
         return TimeFormatter.formatStr(TimeFormatter.Format.SIMPLE,end);
+    }
+
+    public boolean asc(){
+        return "".equals(sort);
     }
 }
--- permission-shiro/src/main/java/com/byc/permission/shiro/service/system/impl/UserServiceImpl.java
+++ permission-shiro/src/main/java/com/byc/permission/shiro/service/system/impl/UserServiceImpl.java
@@ -5,8 +5,10 @@ import com.byc.permission.shiro.service.system.UserService;
 import com.byc.persisent.permission.entity.SysUser;
 import com.byc.persisent.permission.repository.SysUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.criteria.Predicate;
 import java.util.List;
 
 @Service
@@ -23,6 +25,20 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public List<SysUser> findUsers(QueryParam param) {
+        userRepository.findAll((root, query, builder) -> {
+            //条件
+            Predicate predicate = builder.like(root.get("username").as(String.class), String.format("%%s%",param.getCond()));
+            if(param.getStart()!=null)
+                predicate = builder.and(predicate,builder.ge(root.get("create_time").as(Long.class),param.getStart()));
+            if(param.getEnd()!=null)
+                predicate = builder.and(predicate,builder.le(root.get("create_time").as(Long.class),param.getEnd()));
+            //排序
+            if(param.asc())
+                query.where(predicate).orderBy(builder.asc(root.get(param.getField())));
+            else
+                query.where(predicate).orderBy(builder.desc(root.get(param.getField())));
+            return null;
+        },PageRequest.of(1, 10));
         return null;
     }
 }
