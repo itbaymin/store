92cda6b9d724165f9df6c9e7a3e560da8ef89a3e
baiyongcheng <byc996948901>
2020-04-12 19:19:00 +0800

    修改菜单

--- permission-security/src/main/java/com/byc/permission/security/controller/LoginController.java
+++ permission-security/src/main/java/com/byc/permission/security/controller/LoginController.java
@@ -3,6 +3,7 @@ package com.byc.permission.security.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 
@@ -20,6 +21,10 @@ public class LoginController {
     public String logout() {
         return "login";
     }
+
+    @PostMapping(value = "/login")
+    @ResponseBody
+    public void dologin() {}
 
     @GetMapping("/403")
     @ResponseBody
